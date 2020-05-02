d992441b846c4463f172b8cd9f6868566e775b4b
Brian Dearing <smartsnake1@gmail.com>
2020-05-01 19:00:19 -0500

    New IG Checker

--- test/InstagramScraper.py
+++ test/InstagramScraper.py
@@ -1,23 +1,14 @@
-import requests
 import json
-from pymongo import MongoClient
-import time
-import os
+ 
+import requests
 from bs4 import BeautifulSoup
 from random import choice
  
-
-#token = '6ha3tGKyJqphGt43ATmGPq2CE'
-
-#runRequestURL = f'https://api.apify.com/v2/acts/jaroslavhejlek~instagram-scraper/run-sync?token={token}'
-#getDataURL = f'https://api.apify.com/v2/acts/jaroslavhejlek~instagram-scraper/runs/last/dataset/items?token={token}'
-
-debug = True
-
 _user_agents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
 ]
-
+ 
+ 
 class InstagramScraper:
  
     def __init__(self, user_agents=None, proxy=None):
@@ -82,71 +73,25 @@ class InstagramScraper:
                     results.append(node)
         return results
 
+Running = True
+while Running:
+    #self.getIGOnce()
+    
+    IS = InstagramScraper()
+    data = IS.profile_page_recent_posts("https://www.instagram.com/doomsdaycoffee/")
 
-class IGUtil:
-    def __init__(self, config):
-        self.client = MongoClient(os.environ['MONGODB_HOSTNAME'], 27017)#os.environ['MONGODB_HOSTNAME']#config['mongo_url']
-        self.db = self.client[config['database']]
-        self.home_collection = self.db[config['Home_Collection']]
-        self.data = None
+    cleanData = []
+    for d in data:
+        cleanData.append({k: d[k] for k in ("taken_at_timestamp", "display_url", "edge_media_to_caption")})
+    return cleanData
 
-    def resetHomeScreen(self):
-        self.home_collection.drop()
-        self.home_collection.insert_many(self.data)
+    # print(data)
+    #with open('data.json', 'w') as outfile:
+    #    json.dump(data, outfile)
+    time.sleep(1800)#Wait for 30 mins
+#print(f'len: {len(cleanData)}')
 
-    def getLastRun(self):
-        responseData = requests.get(getDataURL, headers={"Content-Type": "application/json"})
-        if debug:
-            print(f'Response: {responseData}')
-        if responseData.ok:
-            data = json.loads(responseData.content)
-            cleanData = []
-            for d in data:
-                cleanData.append({k: d[k] for k in ("timestamp", "url", "imageUrl", "firstComment")})
-            return cleanData
-
-        else:
-            print(f'Error: {responseData.content}')
-            return None
-
-    def newGetIGOnce(self):
-        IS = InstagramScraper()
-        data = IS.profile_page_recent_posts("https://www.instagram.com/doomsdaycoffee/")
-
-        cleanData = []
-        for d in data:
-            cleanData.append({k: d[k] for k in ("taken_at_timestamp", "display_url", "edge_media_to_caption")})
-
-        self.data = cleanData
-        self.resetHomeScreen()
-
-    def getIGOnce(self):
-        if debug:
-            print(f'URL: {runRequestURL}')
-
-        body = json.loads('''{
-          "search": "Doomsday Coffee Cantina",
-          "searchType": "user",
-          "resultsType": "posts",
-          "proxy": {
-            "useApifyProxy": true
-          }
-        }''')
-        response = requests.post(runRequestURL, headers={"Content-Type": "application/json"}, json=body)
-        if debug:
-            print(f'Response: {response}')
-        if response.ok:
-            if debug:
-                print(f'URL: {getDataURL}')
-                cleanData = self.getLastRun()
-                self.data = cleanData
-                self.resetHomeScreen()
-
-        else:
-            print(f'Error: {response.content}')
-
-    def getIGforHomeScreen(self):
-        while True:
-            self.newGetIGOnce()
-            print('test..')
-            time.sleep(30)#1800)#Wait for 30 mins
+    #for index, element in enumerate(cleanData):
+    #index = 0
+    #print(f'index: {index}, data: {(cleanData[index])}')
+    #Running = False
